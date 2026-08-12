3c1760d982497b869e1b080fed9cd5b884da39f1
Bryana Ojopi Banegas <ojopibryana5@gmail.com>
2026-08-12 08:20:29 -0400

    formulario creado

--- app/pages/login.tsx
+++ app/pages/login.tsx
@@ -4,6 +4,11 @@ export default function Login() {
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
         <h1>Esta es la pantalla del login</h1>
+        <form action="">
+            <input type="text" placeholder="Usuario" />
+            <input type="password" placeholder="Contraseña" />
+            <button type="submit">Iniciar sesión</button>
+        </form>
       </main>
     </div>
   );
