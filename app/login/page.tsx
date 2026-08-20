273c8ea5ba9ae360ff53a474b9645863a057f954
Cesar Daniel Antelo Herrera <cesitar.ah@gmail.com>
2026-08-20 19:15:02 -0400

    agregue la estructura completa y todo lo que teniamos hasta la clase de la mañana

---
+++ app/login/page.tsx
@@ -0,0 +1,21 @@
+"use client";
+export default function Login() {
+  return (
+    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
+        <div>
+            <div>
+                <form action="">
+                <input type="text" placeholder="Usuario" />
+                <input type="password" placeholder="Contraseña" />
+                <button className="bg-[#313638] text-white">Ingresar</button>
+        </form>
+            </div>
+        </div>
+        <div>
+            <h1>Este es el login de la pagina</h1>
+        </div>
+      </main>
+    </div>
+  );
+}
