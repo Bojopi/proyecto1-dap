65096d2536cdd35303fab533fee5d4aefe014a17
Cesar Daniel Antelo Herrera <cesitar.ah@gmail.com>
2026-08-20 21:53:08 -0400

    creacion del registrer y conexion al dashboard

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,9 +1,12 @@
 import { Bubblegum_Sans, Butterfly_Kids } from "next/font/google";
-
+import Link from "next/link";
 export default function Register() {
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
+          <div>
+            <h1>Este es el registro de la pagina</h1>
+        </div>
         <div>
             <div>
                 <form action="">
@@ -13,12 +16,14 @@ export default function Register() {
                 <input type="text" placeholder="Usuario" />
                 <input type="password" placeholder="Contraseña" />
                 <input type="password" placeholder="Confirma tu Contraseña" />
-                <button>Registrarse</button>
-        </form>
+              <div className="mt-2">
+              <Link
+                href="/dashboard" className="block w-full rounded-lg px-4 py-2 text-center font-medium bg-[#313638] text-white">
+                Registrarse
+              </Link>
             </div>
-        </div>
-        <div>
-            <h1>Este es el registro de la pagina</h1>
+            </form>
+            </div>
         </div>
       </main>
     </div>
