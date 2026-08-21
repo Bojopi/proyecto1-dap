4985f29b46d72bec9c6809bdef1546d1badbea8c
verdugo191 <alexanderbreton70@gmail.com>
2026-08-20 23:15:07 -0400

    Añadidas rutas de login, register y dashboard con estilos de clase

---
+++ app/pages.tsx/app/datashboard/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+export default function Dashboard() {
+  return (
+    <main className="flex flex-1 w-full flex-col items-center justify-center bg-slate-100">
+      <div className="text-slate-900 flex flex-row w-full items-center justify-around max-w-2xl bg-white h-96 shadow-lg">
+        <div>
+          <h1>Este es el dashboard de la pagina</h1>
+        </div>
+      </div>
+    </main>
+  );
+}
